076f2b79109555be0a78f17bc5ce29e6e4e00516
Mritunjay Mohitesh <mritunjaymohitesh@gmail.com>
2017-12-15 11:00:06 +0530

    Add files via upload

---
+++ abc.php
@@ -0,0 +1,40 @@
+
+
+<html lang="en">
+
+<head>
+<meta charset="utf-8" />
+<meta name="author" content="Script Tutorials" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+<title>HTML5 Audio player with playlist | Script Tutorials</title>
+<!-- add styles and scripts -->
+<link href="css/styles.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="https://www.script-tutorials.com/demos/363/js/jquery-1.7.2.min.js"></script>
+<script type="text/javascript" src="https://www.script-tutorials.com/demos/363/js/jquery-ui-1.8.21.custom.min.js"></script>
+<script type="text/javascript" src="js/main.js"></script>
+</head>
+<div class="player">
+<div class="pl"></div>
+<div class="title"></div>
+<div class="artist"></div>
+<div class="cover"></div>
+<div class="controls">
+<div class="play"></div>
+<div class="pause"></div>
+<div class="rew"></div>
+<div class="fwd"></div>
+</div>
+<div class="volume"></div>
+<div class="tracker"></div>
+</div>
+<ul class="playlist hidden">
+<li audiourl="Brave.mp3">01.mp3</li>
+<li audiourl="23.mp3" >02.mp3</li>
+<li audiourl="Demons.mp3" >03.mp3</li>
+<li audiourl="Roar.mp3" >04.mp3</li>
+<li audiourl="White Walls.mp3">05.mp3</li>
+<li audiourl="Best Day of My Life.mp3">06.mp3</li>
+</ul>
+
+</body>
+</html>
